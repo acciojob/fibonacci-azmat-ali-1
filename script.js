373b9b5ea0c008885c6bbe06700138dd5b6fc57d
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-12 20:36:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	if(num==0){
+		return 1;
+	}
+	if(num==1){
+		return 2;
+	}
+	
+	return fibonacci(num-1)+fibonacci(nums-2);
 }
 
 module.exports = fibonacci;
